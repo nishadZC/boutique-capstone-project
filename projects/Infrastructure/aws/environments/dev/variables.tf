f3fd00845129c8f06841164c532a25c344e0cfcb
nishadZC <vinayak.nishad@zapcg.com>
2026-08-13 12:06:43 +0530

    feat: add infrastructure variable definitions for dev and prod environments

--- projects/Infrastructure/aws/environments/dev/variables.tf
+++ projects/Infrastructure/aws/environments/dev/variables.tf
@@ -62,9 +62,7 @@ variable "disk_size" {
   type        = number
 }
 
-variable "repositories" {
-  type = list(string)
-}
+
 variable "teams_webhook_url" {
   description = "The MS Teams webhook URL for Alertmanager notifications"
   type        = string
