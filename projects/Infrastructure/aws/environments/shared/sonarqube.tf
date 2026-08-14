7a1f0e3c1bf489041d9c0e7a44944e77b7db8810
nishadZC <vinayak.nishad@zapcg.com>
2026-08-14 19:41:34 +0530

    feat: provision SonarQube infrastructure and installation script on AWS EC2

--- projects/Infrastructure/aws/environments/shared/sonarqube.tf
+++ projects/Infrastructure/aws/environments/shared/sonarqube.tf
@@ -58,69 +58,7 @@ resource "aws_instance" "sonarqube" {
   subnet_id     = data.aws_subnets.dev.ids[0]
   vpc_security_group_ids = [aws_security_group.sonarqube.id]
 
-  user_data = replace(<<EOF
-#!/bin/bash
-# Trigger Terraform Cloud Replacement - V2
-
-# Update and install dependencies
-# Ubuntu often runs unattended-upgrades on boot which locks apt. This loop waits for it to finish.
-until apt-get update -y; do sleep 5; done
-until DEBIAN_FRONTEND=noninteractive apt-get install -y openjdk-17-jdk wget unzip acl postgresql postgresql-contrib; do sleep 5; done
-
-# Configure PostgreSQL
-sudo -u postgres psql -c "CREATE USER sonar WITH ENCRYPTED PASSWORD 'sonarpassword';"
-sudo -u postgres psql -c "CREATE DATABASE sonarqube OWNER sonar;"
-
-# Configure sysctl for Elasticsearch
-echo 'vm.max_map_count=262144' >> /etc/sysctl.conf
-echo 'fs.file-max=65536' >> /etc/sysctl.conf
-sysctl -p
-
-# Add sonar user
-groupadd sonar
-useradd -c "Sonar System User" -d /opt/sonarqube -g sonar -s /bin/bash sonar
-
-# Download and extract SonarQube
-SONAR_VERSION="9.9.1.69595"
-cd /tmp
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-$${SONAR_VERSION}.zip
-unzip sonarqube-$${SONAR_VERSION}.zip -d /opt/
-mv /opt/sonarqube-$${SONAR_VERSION} /opt/sonarqube
-chown -R sonar:sonar /opt/sonarqube
-
-# Configure SonarQube JDBC settings
-cat <<EOT >> /opt/sonarqube/conf/sonar.properties
-sonar.jdbc.username=sonar
-sonar.jdbc.password=sonarpassword
-sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube
-EOT
-
-# Create SystemD Service
-cat <<EOT > /etc/systemd/system/sonarqube.service
-[Unit]
-Description=SonarQube service
-After=syslog.target network.target
-
-[Service]
-Type=forking
-ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
-ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
-User=sonar
-Group=sonar
-Restart=always
-LimitNOFILE=65536
-LimitNPROC=4096
-
-[Install]
-WantedBy=multi-user.target
-EOT
-
-# Start and enable SonarQube
-systemctl daemon-reload
-systemctl enable sonarqube
-systemctl start sonarqube
-EOF
-  , "\r", "")
+  user_data = templatefile("${path.module}/sonarqube-setup.sh", {})
 
   tags = {
     Name = "SonarQube-Server"
