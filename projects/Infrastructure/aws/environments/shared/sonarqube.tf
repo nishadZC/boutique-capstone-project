f0815b822295aca9914602f0abb7f6c8d29e5019
nishadZC <vinayak.nishad@zapcg.com>
2026-08-14 17:07:01 +0530

    feat: provision SonarQube EC2 instance and associated security group in shared environment

--- projects/Infrastructure/aws/environments/shared/sonarqube.tf
+++ projects/Infrastructure/aws/environments/shared/sonarqube.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "sonarqube" {
 resource "aws_instance" "sonarqube" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.small"
-  key_name      = "devops-keys"
+  key_name      = "devops-key"
   subnet_id     = data.aws_subnets.dev.ids[0]
   vpc_security_group_ids = [aws_security_group.sonarqube.id]
 
