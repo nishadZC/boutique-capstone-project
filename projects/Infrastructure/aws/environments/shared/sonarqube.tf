73092bc98b8bbab9fb896f259050179297b85b2b
nishadZC <vinayak.nishad@zapcg.com>
2026-08-14 16:25:35 +0530

    feat: provision SonarQube EC2 instance and security group in shared environment

--- projects/Infrastructure/aws/environments/shared/sonarqube.tf
+++ projects/Infrastructure/aws/environments/shared/sonarqube.tf
@@ -1,11 +1,11 @@
-data "aws_vpc" "default" {
-  default = true
+data "aws_vpc" "dev" {
+  cidr_block = "10.1.0.0/16"
 }
 
-data "aws_subnets" "default" {
+data "aws_subnets" "dev" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+    values = [data.aws_vpc.dev.id]
   }
 }
 
@@ -25,7 +25,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_security_group" "sonarqube" {
   name        = "sonarqube-sg"
   description = "Security group for SonarQube"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = data.aws_vpc.dev.id
 
   ingress {
     description = "SonarQube Web UI"
@@ -54,7 +54,7 @@ resource "aws_security_group" "sonarqube" {
 resource "aws_instance" "sonarqube" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.medium"
-  subnet_id     = data.aws_subnets.default.ids[0]
+  subnet_id     = data.aws_subnets.dev.ids[0]
   vpc_security_group_ids = [aws_security_group.sonarqube.id]
 
   tags = {
