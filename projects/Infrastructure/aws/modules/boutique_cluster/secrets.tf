1e899f1237520c6053e96744a1a6a93a9d8af95d
nishadZC <vinayak.nishad@zapcg.com>
2026-08-17 09:57:49 +0530

    Add AWS Secrets Manager configuration and External Secrets Operator Helm deployment

--- projects/Infrastructure/aws/modules/boutique_cluster/secrets.tf
+++ projects/Infrastructure/aws/modules/boutique_cluster/secrets.tf
@@ -1,6 +1,6 @@
 # Create the secret in AWS Secrets Manager
 resource "aws_secretsmanager_secret" "db_password" {
-  name        = "${var.cluster_name}-db-secret-v2"
+  name        = "${var.cluster_name}-db-secret-v3"
   description = "Database password for boutique environment"
 }
 
