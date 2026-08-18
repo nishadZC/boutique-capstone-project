a029b7daf126abba2ecca083d84a21644d65489b
nishadZC <vinayak.nishad@zapcg.com>
2026-08-18 14:59:40 +0530

    feat: add auth service Dockerfile, define product-service K8s resources, and update traffic simulation script to use concurrent workers

--- simulate-traffic.sh
+++ simulate-traffic.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-URL="http://k8s-ingressn-ingressn-6715fec299-81d68e11925ca2b2.elb.us-east-1.amazonaws.com/"
+URL="http://k8s-boutique-frontend-d0e8c9a776-1892332005.us-east-1.elb.amazonaws.com/api/products"
 echo "Sending traffic to $URL to trigger HighTraffic alert..."
 echo "Press Ctrl+C to stop."
 
-while true; do
-  curl -s -o /dev/null -w "Status Code: %{http_code}\n" "$URL"
-  # Sleep for 0.05 seconds = ~20 requests per second.
-  # This is enough to trigger the >10 req/s alert we configured.
-  sleep 0.05
+# Launch 50 concurrent workers that never stop
+for i in {1..50}; do
+  while true; do
+    curl -s -o /dev/null "$URL"
+  done &
 done
+
+# Wait indefinitely
+wait
